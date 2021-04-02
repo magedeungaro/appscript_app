4e4f53c1fa9163874d166773fc2dcab840da08a7
mage deungaro <maria.borgato@alumni.usp.br>
2021-04-02 01:08:17 -0300

    replace.sh agora faz o procedimento em todos os arquivos html na pasta html

--- replace.sh
+++ replace.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 #script para substituir as server side tags do appscript (.gs)
-# Checar se o parâmetro (arquivo) existe
-if [ $1 != "" ]; then
-# Mudar &lt; para < e &gt; para >
-awk '{sub("&lt;","<")}1' $1 > temp.txt && mv temp.txt $1
-awk '{sub("&gt;",">")}1' $1 > temp.txt && mv temp.txt $1
-fi
+# # Checar se o parâmetro (arquivo) existe
+# if [ $1 != "" ]; then
+# # Mudar &lt; para < e &gt; para >
+# awk '{sub("&lt;","<")}1' $1 > temp.txt && mv temp.txt $1
+# awk '{sub("&gt;",">")}1' $1 > temp.txt && mv temp.txt $1
+# fi
+
+for FILE in ./client/html/*.html;
+do 
+  awk '{sub("&lt;","<")}1' $FILE > temp.txt && mv temp.txt $FILE
+  awk '{sub("&gt;",">")}1' $FILE > temp.txt && mv temp.txt $FILE
+done
