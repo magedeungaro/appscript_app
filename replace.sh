3c4e6605249cde5b7ee48e03726ecccbbee0cf58
mage deungaro <maria.borgato@alumni.usp.br>
2021-04-02 13:34:00 -0300

    refactor components - pug

--- replace.sh
+++ replace.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 #script para substituir as server side tags do appscript (.gs)
-# # Checar se o parâmetro (arquivo) existe
-# if [ $1 != "" ]; then
 # # Mudar &lt; para < e &gt; para >
-# awk '{sub("&lt;","<")}1' $1 > temp.txt && mv temp.txt $1
-# awk '{sub("&gt;",">")}1' $1 > temp.txt && mv temp.txt $1
-# fi
 
 for FILE in ./client/html/*.html;
 do 
